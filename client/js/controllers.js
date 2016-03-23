0a374181a0b8e455febacce5acbda4228057b5eb
Tom Moore <tom5265@gmail.com>
2016-03-23 00:02:37 -0500

    everything but last task working

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,11 +1,25 @@
 var controllers = angular.module('myApp.controllers', []);
 
 controllers.controller('TaskController', ['$scope', function($scope) {
-    $scope.ping = function () {
+    $scope.ping = function() {
         alert('You shall not pass!');
     }
-    
-    $scope.texter = function () {
+
+    $scope.texter = function() {
         alert($scope.msg);
     }
+    $scope.randomColor = function() {
+        var numerals = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += numerals[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    $scope.spanner = function() {
+        var newSpan = angular.element('<span> Tom Moore </span>');
+        angular.element(newSpan).appendTo('#spanDiv');
+    }
 }]);
+
--- client/js/main.js
+++ client/js/main.js
@@ -6,3 +6,12 @@ app.config(['$routeProvider', function($routeProvider) {
             controller: 'TaskController'
         });
 }]);
+
+ function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
